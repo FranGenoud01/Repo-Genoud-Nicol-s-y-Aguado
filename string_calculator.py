68eaa948ce35b9eddda80a07ed0c85657ee15319
Franco <francogenoud1@gmail.com>
2026-05-11 22:01:51 -0300

    TDD P6: Soporte para delimitador personalizado

--- string_calculator.py
+++ string_calculator.py
@@ -2,5 +2,14 @@ def sumar(numeros: str) -> int:
     if not numeros:
         return 0
     
-    numeros_limpios = numeros.replace("\n", ",")
+    delimitador = "," 
+    
+    if numeros.startswith("//"):
+        partes = numeros.split("\n", 1) 
+        encabezado = partes[0]  
+        delimitador = encabezado[2:]  
+        numeros = partes[1]  
+    
+    numeros_limpios = numeros.replace("\n", ",").replace(delimitador, ",")
+    
     return sum(int(n) for n in numeros_limpios.split(","))
