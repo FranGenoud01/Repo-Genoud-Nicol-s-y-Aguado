b56dbfeb6954ed7b92d4270cb8d68da9433cd3e1
Franco <francogenoud1@gmail.com>
2026-05-11 21:40:05 -0300

    Correcciones varias

--- string_calculator.py
+++ string_calculator.py
@@ -1,8 +1,7 @@
 def sumar(numeros: str) -> int:
-    def sumar(numeros: str) -> int:
     # Si el string está vacío, retorna 0
-        if not numeros:
-            return 0
-        
-        # Divide por comas, convierte a entero y suma todo
-        return sum(int(n) for n in numeros.split(","))
+    if not numeros:
+        return 0
+    
+    # Divide por comas, convierte a entero y suma todo
+    return sum(int(n) for n in numeros.split(","))
